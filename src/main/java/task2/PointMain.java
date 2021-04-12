44bfbc7f05e677d8758752be8e5f0d3490397871
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-12 12:18:12 +0300

    rebuild

---
+++ src/main/java/task2/PointMain.java
@@ -0,0 +1,10 @@
+package task2;
+
+public class PointMain {
+    public static void main(String[] args) {
+        Point p1 = new Point(4,8);
+        Point p2 = new Point(9,5);
+        System.out.println(p1.distance(p1,p2));
+
+    }
+}
