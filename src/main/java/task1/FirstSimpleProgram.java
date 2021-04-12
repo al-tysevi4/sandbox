a3b2c6d3ebf5a7ce90880417bec4a1950914d538
al-tysevi4 <71289536+al-tysevi4@users.noreply.github.com>
2021-04-12 12:16:46 +0300

    rebuild

---
+++ src/main/java/task1/FirstSimpleProgram.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class FirstSimpleProgram {
+    public static void main(String[] args) {
+        saySomething("world");
+        saySomething("Alex");
+    }
+    public static void saySomething(String s) {
+        System.out.println("Hello, " + s + "!");
+    }
+}
